8040b045826b5007222ad2cc67208e36a20827ac
Thalisson <occtober456@outlook.com>
2021-09-29 22:14:56 -0300

    aa

--- app/src/main/java/com/tcc/webserviceandroid/MainActivity.java
+++ app/src/main/java/com/tcc/webserviceandroid/MainActivity.java
@@ -10,12 +10,16 @@ import android.widget.Button;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -63,8 +67,56 @@ public class MainActivity extends AppCompatActivity {
                         }
 
 
-                )
-            }
+                );
+                requisicao.add(config); // executa a requisicao no webservice
+
         });
+
+        Button cadastrar = findViewById(R.id.cadastrar);
+        cadastrar.setOnClickListiner(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "http://10.87.106.26/thalis/api/Produto";
+                RequestQueue requisicao = Volley.newRequestQueue(MainActivity.this);
+
+                StringRequest config = new StringRequest(
+                        Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        }
+                ){
+                    @Override //Configura o Body para usar JSON
+                    public String getBodyContentType(){
+                        return "application/json; charset=utf-8";
+                    }
+                    @Override //Configura os dados a serem enviados
+                    public byte[] getBody(){
+                        try {
+                            //Criar um objeto do tipo JSON para enviar
+                            JSONObject obj = new JSONObject();
+                            obj.put("id", 9991);
+                            obj.put("nome", "android teste");
+                            obj.put("preco", 99.99);
+                            obj.put("categoria", "cat teste android");
+                            return obj.toString().getBytes(StandardCharsets.UTF_8);
+                        }catch (Exception exc){
+                            exc.printStackTrace();
+                            return null;
+                        }
+                    }
+
+                }
+            }
+
+            }
+        })
     }
-}
